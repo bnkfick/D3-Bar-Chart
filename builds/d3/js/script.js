c207ac91560c4c9fecb54dfa0b9c09212708d287
Barbara Fick <bkantzos@yahoo.com>
2019-07-19 12:02:40 -0600

    use a linear scale for the y axis and map the data to the available physical space.  this will adjust bar height according to the data values, basically drawing all bars proportional to the greatest data value.

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -39,9 +39,7 @@ d3.select('#d3draw svg')
   .style('fill', '#840043');
 
 
- 
-
-  d3.select('#d3draw svg') 
+d3.select('#d3draw svg') 
     .append('text')
     .style("fill", "white")   
     .style("font-family", "Montserrat")   
@@ -51,26 +49,30 @@ d3.select('#d3draw svg')
     .text("ficktastic"); 
 
 
-    var bardata = [20, 30, 45, 15];
-    var height = 400,
-        width = 600,
-        barWidth = 50,
-        barOffset = 5;
-    
-    d3.select('#viz').append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .style('background', '#C9D7D6')
-    .selectAll('rect').data(bardata)
-      .enter().append('rect')
-        .style('fill', '#C61C6F')
-        .attr('width', barWidth)
-        .attr('height', function(d) {
-          return d;
-        })
-        .attr('x', function(d, i) {
-          return i*(barWidth + barOffset)
-        })
-        .attr('y', function(d) {
-          return height - d;
-        });
+    var bardata = [12, 78, 45, 15];
+var height = 400,
+    width = 600,
+    barWidth = 50,
+    barOffset = 5;
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(bardata)])
+    .range([0,height]);
+
+d3.select('#viz').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .style('background', '#C9D7D6')
+.selectAll('rect').data(bardata)
+  .enter().append('rect')
+    .style('fill', '#C61C6F')
+    .attr('width', barWidth)
+    .attr('height', function(d) {
+      return yScale(d);
+    })
+    .attr('x', function(d, i) {
+      return i*(barWidth + barOffset)
+    })
+    .attr('y', function(d) {
+      return height - yScale(d);
+    });
