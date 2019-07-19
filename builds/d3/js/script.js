b87cc4dcaf3585963be3f99b454282978d67a395
Barbara Fick <bkantzos@yahoo.com>
2019-07-19 10:36:57 -0600

    using data binding for table data

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,21 +1,17 @@
-d3.selectAll("tr:nth-child(4) .day-high .temp")
-.append('span')
-.html('hot')
-// .style('background', 'red')
-// .style('padding', '3px')
- .style('margin-left', '3px')
-// .style('border-radius', '3px')
-// .style('color', 'white')
- .classed('label label-danger', true)
- .classed('labe-danger', false)
- .classed('label-warning', true)
- .style('margin-left', '3px')
-
- d3.select('h2 span')
- .classed('small', false)
-
- d3.selectAll('tr')
- .insert('td', ':first-child')
- .append('input')
- .attr('type', 'checkbox')
- .property('checked', true)
+var mydata = [
+    { date: '6/01/2019', low: 55, high: 78 },
+    { date: '6/02/2019', low: 65, high: 83 },
+    { date: '6/03/2019', low: 77, high: 90 },
+    { date: '6/04/2019', low: 58, high: 78 },
+    { date: '6/05/2019', low: 67, high: 92 },
+  ];
+  
+  d3.select('tbody')
+    .selectAll('tr')
+    .data(mydata)
+    .enter().append('tr')
+    .html(function(d) {
+      return '<th scope="row">' + d.date +
+              '</th><td>' + d.low +
+              '</td><td>' + d.high + '</td>'
+    })
