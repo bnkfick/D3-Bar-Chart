5752700deec680ee773fe178944b9db9c96e5c58
Barbara Fick <bkantzos@yahoo.com>
2019-07-19 13:20:05 -0600

    added meaningful colors based on temperature.  adjusted tooltip

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -52,9 +52,7 @@ d3.json('js/data/forecast.json', function(d) {
 
     var temperatures = [],
         height = 400,
-        width = 600,
-        barWidth = 50,
-        barOffset = 5;
+        width = 600;
 
     var   tempColor,
             yScale,
@@ -68,38 +66,33 @@ d3.json('js/data/forecast.json', function(d) {
     }
 
     yScale = d3.scaleLinear()
-        .domain([0, d3.max(temperatures)])
-        .range([0,height]);
+    .domain([0, d3.max(temperatures)])
+    .range([0,height]);
 
     xScale = d3.scaleBand()
         .domain(temperatures)
-        .paddingInner(.3)
+        .paddingInner(.1)
         .paddingOuter(.1)
-        .range([0, width])
+        .range([0, width]);
 
     colors = d3.scaleLinear()
-        .domain([0, temperatures.length *.33,
-                    temperatures.length *.66,
-                    temperatures.length
-                    ])
-        .range(['#B58929', '#C61C6F',
-                '#268BD2', '#85992C'])
+        .domain([0, 65, d3.max(temperatures)])
+        .range(['#FFFFFF', '#2D8BCF', '#DA3637']);
 
     tooltip = d3.select('body')
         .append('div')
         .style('position', 'absolute')
-        .style('padding', '0 10px')
+        .style('padding', '0 8px 0 10px')
         .style('background', 'white')
-        .style('opacity', 0);
+        .style('opacity', 0)
+        .style('border-radius', '10px');
 
     myChart = d3.select('#viz').append('svg')
         .attr('width', width)
         .attr('height', height)
         .selectAll('rect').data(temperatures)
         .enter().append('rect')
-        .attr('fill', function(d, i) {
-            return colors(i)
-        })
+        .attr('fill', colors)
         .attr('width', function(d) {
             return xScale.bandwidth();
         })
@@ -112,15 +105,19 @@ d3.json('js/data/forecast.json', function(d) {
         .on('mouseover', function(d) {
             tooltip.transition().duration(200)
             .style('opacity', .9)
-            tooltip.html(d)
-            .style('left', (d3.event.pageX -35) + 'px')
-            .style('top', (d3.event.pageY -30) + 'px')
-            tempColor = this.style.fill;
-            d3.select(this)
-            .style('fill', 'yellow')
-        })
+            tooltip.html(
+                '<div style="font-size: 2rem; font-weight: bold">' +
+                  d + '&deg;</div>'
+              )
+                .style('left', (d3.event.pageX -35) + 'px')
+                .style('top', (d3.event.pageY -30) + 'px')
+              tempColor = this.style.fill;
+              d3.select(this)
+                .style('fill', '#EBCC40')
+            })
 
         .on('mouseout', function(d) {
+            tooltip.html('')
             d3.select(this)
             .style('fill', tempColor)
         });
