5cfbf1d76c9221c62d3314710be3170ede4516a3
Barbara Fick <bkantzos@yahoo.com>
2019-07-19 11:50:30 -0600

    created a simple bar chart.  the end goal is a color coded temperature bar chart.

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -13,7 +13,10 @@ var mydata = [
     .html(function(d) {
       return '<th scope="row">' + d.date +
               '</th><td>' + d.low +
-              '</td><td>' + d.high + '</td>'
+              '&deg; <span class="notation">F' +
+              '</td><td>' + d.high + 
+              '&deg; <span class="notation">F' +
+              '</td>'
     })
 
 d3.select('#d3draw')
@@ -48,4 +51,26 @@ d3.select('#d3draw svg')
     .text("ficktastic"); 
 
 
-
+    var bardata = [20, 30, 45, 15];
+    var height = 400,
+        width = 600,
+        barWidth = 50,
+        barOffset = 5;
+    
+    d3.select('#viz').append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .style('background', '#C9D7D6')
+    .selectAll('rect').data(bardata)
+      .enter().append('rect')
+        .style('fill', '#C61C6F')
+        .attr('width', barWidth)
+        .attr('height', function(d) {
+          return d;
+        })
+        .attr('x', function(d, i) {
+          return i*(barWidth + barOffset)
+        })
+        .attr('y', function(d) {
+          return height - d;
+        });
